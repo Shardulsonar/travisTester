4ddfa22b6cffc120970a2e1e54d2c106c72ac867
Shardulsonar <shardulsonar@gmail.com>
2018-11-22 00:33:33 +0100

    Initial commit

---
+++ src/main/java/studentification/com/srh/studentification/App.java
@@ -0,0 +1,13 @@
+package studentification.com.srh.studentification;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
